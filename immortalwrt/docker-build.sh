b12f890bc611b29878167d8fd6f7ca0cc7708b58
adjlevis <adjlevis@qq.com>
2025-11-08 20:28:09 +0800

    Update docker-build.sh

--- immortalwrt/docker-build.sh
+++ immortalwrt/docker-build.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 set -e
 
-# 确保输出目录存在并可写
-mkdir -p ./bin
-chmod -R 777 ./bin || true
+# 放宽权限，避免 Permission denied
+chmod -R 777 "$(pwd)"
 
 docker run --rm \
-  --user $(id -u):$(id -g) \
-  -v "$(pwd)/bin:/home/build/immortalwrt/bin" \
-  -v "$(pwd)/files:/home/build/immortalwrt/files" \
-  -v "$(pwd)/build.sh:/home/build/immortalwrt/build.sh" \
+  -v "$(pwd):/home/build/immortalwrt" \
+  --user root \
   immortalwrt/imagebuilder:layerscape-armv8_64b-openwrt-24.10 \
   /home/build/immortalwrt/build.sh
+
 
 
 
